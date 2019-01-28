48f8f00214aa70182189e2fb477a8933dd421e4f
Guilherme <portigliotti.guilherme@hotmail.com>
2019-01-28 17:24:26 -0200

    Atividade 04 - adicionando validacoes para o campo de nome e email

--- prova/js/validacao.js
+++ prova/js/validacao.js
@@ -2,9 +2,11 @@ var nome = $("#nome");
 
 function validaNome(){
   nome.on("change", function(){
+  var verifyInt = /\d+/g;
   var valor = nome.val();
   var teste = valor.split(/\S+/).length - 1;
-  if(teste >= 2){
+
+  if(teste >= 2 && $(nome).val().match(verifyInt) == null){
     nome.removeClass("borda-vermelha");
     nome.addClass("borda-verde");
     return true;
@@ -13,10 +15,34 @@ function validaNome(){
     nome.addClass("borda-vermelha");
     return false;
   }
+
 });
 }
-var disponibilidade = $("#disponibilidade");
-disponibilidade.on("click", function(){
-  $("#personalizar").attr("hidden", false);
-  console.log(disponibilidade.val());
+
+validaNome();
+
+
+
+var email = $("#email");
+function validaEmail(){
+  email.on("change", function(){
+  var valor = email.val();
+  var myRe = /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])/;
+
+
+  if(myArray = myRe.exec(valor)){
+    email.removeClass("borda-vermelha");
+    email.addClass("borda-verde");
+
+    return true;
+
+  }else{
+    email.removeClass("borda-verde");
+    email.addClass("borda-vermelha");
+    return false;
+  }
+
 });
+}
+
+validaEmail();
