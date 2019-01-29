a350369c77d768e19f9eebb46bcd063ae35fdf1c
Guilherme <portigliotti.guilherme@hotmail.com>
2019-01-29 17:05:25 -0200

    Atividade 04 - Adicionando vereficações aos campos da inscrição.html e ao botao de 'enviar mensagem'

--- prova/js/validacao.js
+++ prova/js/validacao.js
@@ -1,5 +1,7 @@
 var nome = $("#nome");
 
+let nomeV = false;
+
 function validaNome(){
   nome.on("change", function(){
   var verifyInt = /\d+/g;
@@ -9,21 +11,25 @@ function validaNome(){
   if(teste >= 2 && $(nome).val().match(verifyInt) == null){
     nome.removeClass("borda-vermelha");
     nome.addClass("borda-verde");
+    nomeV = true;
+    habilita();
     return true;
   }else{
     nome.removeClass("borda-verde");
     nome.addClass("borda-vermelha");
+    nomeV = false;
+    habilita();
     return false;
   }
 
 });
 }
-
 validaNome();
 
-
+let emailV = false;
 
 var email = $("#email");
+
 function validaEmail(){
   email.on("change", function(){
   var valor = email.val();
@@ -33,16 +39,78 @@ function validaEmail(){
   if(myArray = myRe.exec(valor)){
     email.removeClass("borda-vermelha");
     email.addClass("borda-verde");
-
+    emailV = true;
+    habilita();
     return true;
 
   }else{
     email.removeClass("borda-verde");
     email.addClass("borda-vermelha");
+    emailV = false;
+    habilita();
     return false;
   }
 
 });
 }
-
 validaEmail();
+
+let interesses = false;
+var outro = $(".outro");
+outro.on("click", function(){
+  interesses = true;
+  habilita();
+  if(outro.is(":checked") == true) {
+    console.log(outro);
+    $("#interesses").attr("hidden", false);
+  } else {
+    $("#interesses").attr("hidden", true);
+  }
+});
+
+let turno = false;
+let disponibilidade = $("input[type='radio']");
+disponibilidade.on("click", function() {
+  turno = true;
+  habilita();
+  if ($(this).val() == "disponibilidade") {
+    console.log(($(this).val()));
+
+    $("#personalizar").attr("hidden", false);
+    return;
+  }
+
+  $("#personalizar").attr("hidden", true);
+
+});
+
+
+
+let areaV = false;
+$(".area").focusout(function() {
+  validaArea($(".area"));
+});
+
+function validaArea(area) {
+  let cont = area.val().length;
+  if (cont > 140) {
+    area.removeClass("borda-vermelha");
+    area.addClass("borda-verde");
+    areaV = true;
+    habilita();
+    return;
+  }
+
+  area.removeClass("borda-verde");
+  area.addClass("borda-vermelha");
+  areaV = false;
+  habilita();
+}
+
+function habilita() {
+  if (disponibilidade && outro  && nomeV && emailV && areaV) {
+    $('.botao').removeClass("disable");
+  } else {
+    $('.botao').addClass("disable");
+  }
+}
